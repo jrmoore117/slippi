20468ffce25f233ddfb4e696e4214ee86392764d
jrmoore117 <jrmoore117@gmail.com>
2020-08-24 08:26:07 -0700

    Add stock icons and console log two from test file.

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ const game = new SlippiGame("test.slp");
 
 // Get game settings – stage, characters, etc
 const settings = game.getSettings();
-console.log(settings);
+const [player1, player2] = settings.players;
+console.log(`stock-icon-${player1.characterId}-${player1.characterColor}`);
+console.log(`stock-icon-${player2.characterId}-${player2.characterColor}`);
 
 // Get metadata - start time, platform played on, etc
 const metadata = game.getMetadata();
-console.log(metadata);
+// console.log(metadata);
 
 // Get computed stats - openings / kill, conversions, etc
 const stats = game.getStats();
-console.log(stats);
+// console.log(stats);
 
 // Get frames – animation state, inputs, etc
 // This is used to compute your own stats or get more frame-specific info (advanced)
 const frames = game.getFrames();
-console.log(frames[0].players); // Print frame when timer starts counting down
+console.log(frames[1000].players); // Print frame when timer starts counting down
 // console.log(frames[1].players);
 // console.log(frames[2].players);
 // console.log(frames[3].players);
